1a4fa990daead068541b5e3b6857ce16974caaae
V Nelson barnes <apple@nelbar.com>
2019-07-17 08:12:34 -0400

    Initial Commit

---
+++ baseWatch1 WatchKit Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  baseWatch1 WatchKit Extension
+//
+//  Created by Linus T Dogg on 7/17/19.
+//  Copyright © 2019 Linus T Dogg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ baseWatch1 WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  baseWatch1 WatchKit Extension
+//
+//  Created by Linus T Dogg on 7/17/19.
+//  Copyright © 2019 Linus T Dogg. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
